2253cc336a7ac093b98831de18b0164249ff5d2d
hirokihello <iammyeye1@gmail.com>
2022-06-12 14:15:11 +0900

    solidjs関連のファイルの削除

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { ipcRenderer } = require('electron');
-
-window.customRenderMethod = async () => {
-  return ipcRenderer.invoke('custom-ready');
-};
-console.log("preloaded")
-console.log("hoge")
